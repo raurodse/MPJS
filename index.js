a81622ee0d15967680f27ce3f8d69c77872f5bf3
Raul Rodrigo <raurodse@gmail.com>
2012-01-10 21:12:42 +0100

    modified server to return handlers functions result

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 var miservidor = require("server");
-miservidor.iniciar();
+var router = require("router");
+var requestHandlers = require("requestHandlers");
+var handler = {}
+handler['/'] = requestHandlers.iniciar;
+handler['/iniciar'] = requestHandlers.iniciar;
+handler['/subir'] = requestHandlers.subir;
+miservidor.iniciar(router.router,handler);
