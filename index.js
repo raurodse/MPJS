62c52a16edf99b27ddff0ed94b877c75c370d6d6
Raul Rodrigo <raurodse@gmail.com>
2012-01-12 00:12:35 +0100

    clean all code

--- index.js
+++
@@ -1,8 +0,0 @@
-var miservidor = require("server");
-var router = require("router");
-var requestHandlers = require("requestHandlers");
-var handler = {}
-handler['/'] = requestHandlers.iniciar;
-handler['/iniciar'] = requestHandlers.iniciar;
-handler['/subir'] = requestHandlers.subir;
-miservidor.iniciar(router.router,handler);
