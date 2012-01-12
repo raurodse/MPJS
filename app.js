bb54620977000f70cab70462192967c1c771ccb6
Raul Rodrigo <raurodse@gmail.com>
2012-01-12 08:27:44 +0100

    push

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-
+app.get('*', routes.notfound);
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,6 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+exports.notfound = function(req, res){
+  res.render('404',{ title: '404 Not Found'})
+};
