b59b8138e0187ceac00661866cae1863f0301bd3
Raul Rodrigo <raurodse@gmail.com>
2012-01-13 22:33:02 +0100

    Now play videos

--- app.js
+++ app.js
@@ -30,8 +30,9 @@ app.configure('production', function(){
 
 // Routes
 app.get('/', routes.index);
-app.get('/view/*', routes.view);
-app.get('*', routes.notfound);
+app.get('/view/(*)?', routes.view);
+app.get('/play/(*)?', routes.play);
+//app.get('*', routes.notfound);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,46 @@
  * GET home page.
  */
 var fs = require('fs');
+var spawn = require('child_process').spawn;
 exports.index = function(req, res){
   res.render('index', { title: 'MPJS' })
 };
 
+
+
+
 exports.view = function(req, res){
-  if (req.params[0] != ''){
-  listafull = fs.readdirSync(req.params[0]);
-  lista = new Array();
-  for (item in listafull)
-	{
-		if (fs.statSync(req.params[0]+"/"+listafull[item]).isDirectory())
-			lista.push(listafull[item]);
-	}
-  res.render('view', { title: 'MPJS', items : lista })}
-  else
+  if (typeof req.params[0] != 'undefined' && req.params[0] != ''){
+    var rootpath = req.params[0];
+    listafull = fs.readdirSync(req.params[0]);
+    var directories = new Array();
+    var files = new Array();
+    for (item in listafull)
+      {
+          if (fs.statSync(req.params[0]+"/"+listafull[item]).isDirectory())
+              directories.push(listafull[item]);
+          else
+              files.push(listafull[item]);
+      }
+      directories.sort();
+      files.sort();
+    res.render('view', { title: 'MPJS',rootpath: rootpath , directories : directories, files : files })
+  }
+  else{
 	res.render('404',{title: '404 Not Found'});
+  }
 };
 
 exports.notfound = function(req, res){
   res.render('404',{ title: '404 Not Found'})
 };
+
+
+exports.play = function(req,res)
+{
+  var command = spawn('mplayer',[req.params[0]]);
+  command.on('exit',function(code){res.end("Finish");})
+  res.writeHead(200,{'Content-Type': 'text/plain'});
+  res.write("Play " + req.params[0] + " ... ");
+}
+
