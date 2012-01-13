b33d56eaf88fdbec4e9c00331ce6f0824472749b
Raul Rodrigo <raurodse@gmail.com>
2012-01-13 01:02:25 +0100

    Work in progress to read files from directory

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
 
+
 var app = module.exports = express.createServer();
 
 // Configuration
@@ -28,8 +29,9 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/', routes.index);
+app.get('/view/*', routes.view);
 app.get('*', routes.notfound);
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@
 /*
  * GET home page.
  */
-
+var fs = require('fs');
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'MPJS' })
 };
+
+exports.view = function(req, res){
+  
+  res.render('view', { title: 'MPJS', items : lista })
+};
+
 exports.notfound = function(req, res){
   res.render('404',{ title: '404 Not Found'})
 };
